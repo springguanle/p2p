734d4b4c84ef99de072d456024fcb3f6bee24364
bj <bj@126.com>
2019-01-15 17:13:34 +0800

    historyAverageRate

--- p2p-dataservice/src/main/java/com/bjpowernode/p2p/service/LoanInfoServiceImpl.java
+++ p2p-dataservice/src/main/java/com/bjpowernode/p2p/service/LoanInfoServiceImpl.java
@@ -1,22 +1,38 @@
 package com.bjpowernode.p2p.service;
 
+import com.bjpowernode.p2p.constant.Constants;
 import com.bjpowernode.p2p.mapper.loan.LoanInfoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.stereotype.Service;
 
 /**
  * @author zhangguanle
  * @create 2019-01-15 1:15
  */
-@Service
+@Service("loanInfoServiceImpl")
 public class LoanInfoServiceImpl implements LoanInfoService {
+
+    @Autowired
+    private RedisTemplate<Object,Object> redisTemplate;
 
     @Autowired
     private LoanInfoMapper loanInfoMapper;
 
     @Override
     public Double queryHistoryAverageRate() {
-        Double historyAverageRate=loanInfoMapper.queryHistoryAverageRate();
+        //先从缓冲中取出
+        //修改key的序列化
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        Double  historyAverageRate= (Double) redisTemplate.opsForValue().get(Constants.HISTORY_AVERAGE_RATE);
+
+        if(historyAverageRate==null){
+            historyAverageRate=loanInfoMapper.queryHistoryAverageRate();
+            //放到缓存中
+            redisTemplate.opsForValue().set(Constants.HISTORY_AVERAGE_RATE,historyAverageRate);
+        }
+
         return historyAverageRate;
     }
 }
