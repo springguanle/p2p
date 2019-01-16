1746f0660565a6d5a2bd18bc347692a27ab8161f
bj <bj@126.com>
2019-01-16 21:33:54 +0800

    product index

---
+++ p2p-web/src/main/java/com/bjpowernode/p2p/controller/LoanInfoController.java
@@ -0,0 +1,16 @@
+package com.bjpowernode.p2p.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+/**
+ * @author zhangguanle
+ * @create 2019-01-16 19:28
+ */
+@Controller
+public class LoanInfoController {
+
+    public  String loan(Integer productType, Integer currentPage, Model model){
+        return null;
+    }
+}
